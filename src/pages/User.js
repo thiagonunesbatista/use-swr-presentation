33beeefbe7124a3f3496d7351c27e43f43f95254
Thiago Nunes Batista <67487679+thiagonunesbatista@users.noreply.github.com>
2021-03-16 20:02:15 -0300

    remove api keys

--- src/pages/User.js
+++ src/pages/User.js
@@ -7,19 +7,15 @@ import Loader from 'components/Loader'
 
 const User = () => {
   const { login } = useParams()
+
   const { data: user } = useSWR(
     `get-user-${login}`,
-    async () =>
-      await axios(
-        `https://api.github.com/users/${login}?client_secret=e7c89ee73f1e6bacbbbd6cb71333d3e509ec3b8a&client_id=c347e10e86a72a8ade32`
-      )
+    async () => await axios(`https://api.github.com/users/${login}`)
   )
 
   if (!user) {
     return <Loader />
   }
-
-  console.log(user)
 
   return (
     <UserCard>
--- src/services/users.js
+++ src/services/users.js
@@ -1,21 +1,15 @@
 import axios from 'axios'
 
 export const getUser = username =>
-  axios(
-    `https://api.github.com/users/${username}?client_secret=e7c89ee73f1e6bacbbbd6cb71333d3e509ec3b8a&client_id=c347e10e86a72a8ade32`
-  )
+  axios(`https://api.github.com/users/${username}`)
 
 export const getUserRepos = (username, perPage = 8) =>
-  axios(
-    `https://api.github.com/users/${username}/repos?per_page=${perPage}&client_secret=e7c89ee73f1e6bacbbbd6cb71333d3e509ec3b8a&client_id=c347e10e86a72a8ade32`
-  )
+  axios(`https://api.github.com/users/${username}/repos?per_page=${perPage}`)
 
 export const searchUsers = (searchParam, perPage = 8) =>
   axios(
-    `https://api.github.com/search/users?q=${searchParam}&per_page=${perPage}&client_secret=e7c89ee73f1e6bacbbbd6cb71333d3e509ec3b8a&client_id=c347e10e86a72a8ade32`
+    `https://api.github.com/search/users?q=${searchParam}&per_page=${perPage}`
   )
 
 export const showUsers = (perPage = 8) =>
-  axios(
-    `https://api.github.com/users?per_page=${perPage}&client_secret=e7c89ee73f1e6bacbbbd6cb71333d3e509ec3b8a&client_id=c347e10e86a72a8ade32`
-  )
+  axios(`https://api.github.com/users?per_page=${perPage}`)
